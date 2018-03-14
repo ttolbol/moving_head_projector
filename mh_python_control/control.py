1141ddeac3de0ce9f05c0adf402fbf4f276ae533
Jóhann <johann.bjorn.bjornsson@gmail.com>
2018-03-14 22:41:35 +0100

    Adding support for joystick.. not there yet

--- mh_python_control/control.py
+++ mh_python_control/control.py
@@ -1,19 +1,49 @@
 from Cameo25 import Cameo25
+import pygame
 import time
 
 
-cameo = Cameo25(115200, debug=True)
+#cameo = Cameo25(115200, debug=True)
 
-#Ramp up the speed
-for i in range(10, 350, 5):
-    cameo.move_x(i)
-    time.sleep(0.02)
+# Get count of joysticks
+pygame.init()
+pygame.joystick.init()
 
-#Ramp down
-for i in range(350, 10, -5):
-    cameo.move_x(i)
-    time.sleep(0.02)
+joystick = pygame.joystick.Joystick(0)  # take the first one
+joystick.init()
 
-cameo.stop_all()
+old_e = None
+while True:
+    time.sleep(1/60)  # 60 hertz refresh rate
+    events = pygame.event.get()
 
-cameo.die()
+    if len(events) != 0:
+        # events are waiting in the queue
+
+        last_x = None
+        last_y = None
+
+        # we get the latest of the events from the queue
+        for e in events:
+            if e.type == 7:  # 7 is JOYAXISMOTION
+                if e.__dict__["axis"] == 0:  # x axis
+                    last_x = e
+                if e.__dict__["axis"] == 1:  # y axis
+                    last_y = e
+
+        if last_x is not None:
+            print("x: " + str(last_x.__dict__["value"]))
+
+        if last_y is not None:
+            print("y: " + str(last_y.__dict__["value"]))
+
+# # For each joystick:
+# for i in range(joystick_count):
+#     joystick = pygame.joystick.Joystick(i)
+#     joystick.init()
+
+#cameo.stop_all()
+
+#cameo.die()
+
+pygame.quit()
